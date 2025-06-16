1d944373a968fed4b286db0f9f3ef4741be7bd43
denismitali17 <d.mitali1@alustudent.com>
2025-06-16 19:57:46 +0200

    Probability and Statistics Fundamentals

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -1,55 +1,89 @@
 #!/usr/bin/env python3
-'''
-    Poisson distribution
-    that represents a poisson distribution
-'''
+""" defines Poisson class that represents Poisson distribution """
+
+
 class Poisson:
-    '''
-        Class Poisson that represents a
-        distribution of Poisson
-    '''
-    def factorial(self, k):
-        '''
-            Calculates the factorial
-        '''
-        if k < 0:
-            return 0
-        if k == 0 or k == 1:
-            return 1
-        return k * self.factorial(k - 1)
+    """
+    class that represents Poisson distribution
+
+    class constructor:
+        def __init__(self, data=None, lambtha=1.)
+
+    instance attributes:
+        lambtha [float]: the expected number of occurances in a given time
+
+    instance methods:
+        def pmf(self, k): calculates PMF for given number of successes
+        def cdf(self, k): calculates CDF for given number of successes
+    """
 
     def __init__(self, data=None, lambtha=1.):
-        '''
-            Class constructor
-        '''
+        """
+        class constructor
+
+        parameters:
+            data [list]: data to be used to estimate the distibution
+            lambtha [float]: the expected number of occurances on a given time
+
+        Sets the instance attribute lambtha as a float
+        If data is not given:
+            Use the given lambtha or
+            raise ValueError if lambtha is not positive value
+        If data is given:
+            Calculate the lambtha of data
+            Raise TypeError if data is not a list
+            Raise ValueError if data does not contain at least two data points
+        """
         if data is None:
-            if lambtha <= 0:
-                raise ValueError('lambtha must be a positive value')
-            self.lambtha = float(lambtha)
+            if lambtha < 1:
+                raise ValueError("lambtha must be a positive value")
+            else:
+                self.lambtha = float(lambtha)
         else:
             if type(data) is not list:
-                raise TypeError('data must be a list')
-            if len(data) < 2:
-                raise ValueError('data must contain multiple values')
-            self.lambtha = float(sum(data) / len(data))
+                raise TypeError("data must be a list")
+            elif len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            else:
+                lambtha = float(sum(data) / len(data))
+                self.lambtha = lambtha
 
     def pmf(self, k):
-        '''
-            Calculates the value of the
-            PMF for a given number of successes
-        '''
+        """
+        calculates the value of the PMF for a given number of successes
+
+        parameters:
+            k [int]: number of successes
+                If k is not an int, convert it to int
+                If k is out of range, return 0
+
+        return:
+            the PMF value for k
+        """
+        if type(k) is not int:
+            k = int(k)
         if k < 0:
             return 0
-        k = int(k)
         e = 2.7182818285
-        return ((self.lambtha ** k) * (e ** (-self.lambtha))
-                ) / (self.factorial(k))
+        lambtha = self.lambtha
+        factorial = 1
+        for i in range(k):
+            factorial *= (i + 1)
+        pmf = ((lambtha ** k) * (e ** -lambtha)) / factorial
+        return pmf
 
     def cdf(self, k):
-        '''
-            Calculates the value of the
-            CDF for a given number of successes
-        '''
+        """
+        calculates the value of the CDF for a given number of successes
+
+        parameters:
+            k [int]: number of successes
+                If k is not an int, convert it to int
+                If k is out of range, return 0
+
+        return:
+            the CDF value for k
+        """
         if type(k) is not int:
             k = int(k)
         if k < 0:
@@ -57,4 +91,4 @@ class Poisson:
         cdf = 0
         for i in range(k + 1):
             cdf += self.pmf(i)
-        return cdf      
+        return cdf
