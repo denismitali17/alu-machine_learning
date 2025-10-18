7de3e19416f01da842ba6f6819c430cf17c32858
denismitali17 <d.mitali1@alustudent.com>
2025-10-19 00:05:59 +0200

    APIs

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -1,41 +1,30 @@
 #!/usr/bin/env python3
-"""
-    script that prints the location of a specific user:
-"""
-
-
 import requests
+import sys
 import time
-from datetime import datetime
-
 
 def main(url):
-    """
-    - The user is passed as first argument of the script
-    with the full API URL, example: ./2-user_location.py
-    https://api.github.com/users/holbertonschool
-    - If the user doesn’t exist, print Not found
-    - If the status code is 403, print Reset in X min where X
-    is the number of minutes from now and the value of
-    X-Ratelimit-Reset
-    - Your code should not be executed when the file is
-    imported (you should use if __name__ == '__main__':)
-
-    """
     response = requests.get(url)
 
     if response.status_code == 404:
         print("Not found")
     elif response.status_code == 403:
-        reset_timestamp = int(response.headers["X-Ratelimit-Reset"])
-        current_timestamp = int(time.time())
-        reset_in_minutes = (reset_timestamp - current_timestamp) // 60
-        print("Reset in {} min".format(reset_in_minutes))
+        reset_time = response.headers.get("X-RateLimit-Reset")
+        if reset_time:
+            reset_timestamp = int(reset_time)
+            minutes_left = (reset_timestamp - int(time.time())) // 60
+            print(f"Reset in {minutes_left} min")
+        else:
+            print("Reset in unknown time")
+    elif response.status_code == 200:
+        data = response.json()
+        location = data.get("location")
+        print(location if location else "Not found")
     else:
-        print(response.json()["location"])
-
+        print("Error: Unexpected response")
 
 if __name__ == "__main__":
-    import sys
-
-    main(sys.argv[1])
+    if len(sys.argv) < 2:
+        print("Usage: python 2-user_location.py https://api.github.com/users/holbertonschool")
+    else:
+        main(sys.argv[1])
